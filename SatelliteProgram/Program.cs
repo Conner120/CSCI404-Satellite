bfd59ddfc6247a2df0cf78f4e9560cacedfbf58a
conner <Conner@reitmeier.org>
2021-05-02 13:40:30 -0500

    sat prog

--- SatelliteProgram/Program.cs
+++ SatelliteProgram/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using WebSocketSharp;
 using WebSocketSharp.Server;
 
@@ -6,28 +7,49 @@ namespace Satellite
 {
   public class Commands : WebSocketBehavior
   {
+    public List<string> convergence_name;
+    public String satellite_name = "";
+    public bool waiting_confirmation = false;
     protected override void OnMessage (MessageEventArgs e)
     {
         if (e.Data.StartsWith("COMMAND"))
         {
-            // if(trusted)
-                var msg = "running";
+          if(waiting_confirmation){
+            if(false){//if(trusted)
+                Send("Running CMD");
                 //output orbital change request
-            // else 
+            }else{ 
                 // waiting for second confirmation or one time pass 
-            
+                waiting_confirmation = true;
+                convergence_name = new List<string>();
+                convergence_name.InsertRange(0,e.Data.Split(" ")[1].Split(","));
+                Send("Getting Confirmation OR send override");
+            }
+          }else{
+            if(e.Data.Split(" ").Length>2){
+              //process pad overide
+            }
+          }
         }else if(e.Data.StartsWith("SECONDREQUEST")){
             var msg = "SECOND intercept {sat name} {time}";
+            Send("OK");
         }else if(e.Data.StartsWith("SECONDRESPONSE")){
-            // if(confimed) 
+            Send("OK");
+            if(e.Data.Split(" ")[1]=="TRUE"){
             //      take action ordered
-            // else
+            }else{
             //      reject and degrade trust model 
+            }
         } else if(e.Data.StartsWith("GROUNDCONNECTED")){
             //tell ground if any actions failed
             // add connection to trust model
+            if(convergence_name.Count>0){
+              Send($"CONVERGENCE {String.Join(",",convergence_name)} {satellite_name}");
+              convergence_name=new List<string>();
+            }else{
+              Send("OK");
+            }
         }
-      Send (msg);
     }
   }
 

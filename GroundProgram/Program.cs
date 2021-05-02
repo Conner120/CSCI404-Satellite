97190dfb6eaa66299c6aeab61c4061161d5e021d
conner <Conner@reitmeier.org>
2021-05-02 15:42:18 -0500

    comm structure

--- GroundProgram/Program.cs
+++ GroundProgram/Program.cs
@@ -1,20 +1,67 @@
 ﻿using System;
 using WebSocketSharp;
 using System.Threading;
-namespace Ground
+using SGPdotNET.CoordinateSystem;
+using SGPdotNET.Observation;
+using SGPdotNET.TLE;
+using SGPdotNET.Util;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;namespace Ground
 {
   public class Program
   {
     public static void Main (string[] args)
     {
-      using (var ws = new WebSocket ("ws://localhost:1234/Commands")) {
-        ws.OnMessage += (sender, e) =>
-            Console.WriteLine ("Commands says: " + e.Data);
+      var satellites = new List<Satellite>();
+        try
+        {
+            // Open the text file using a stream reader.
 
-        ws.Connect ();
-        ws.Send ("BALUS");
-        Thread.Sleep(25);
-      }
+                var file = new StreamReader("OrbitalPositionTest/data/all.tle").ReadToEnd(); // big string
+                var lines = file.Split(new char[] {'\n'});           // big array
+                var count = lines.Length;
+                for (int i = 0; i < count; i+=3)
+                {
+                    var l1 = lines[i];
+                    var l2 = lines[i+1];
+                    var l3 = lines[i+2];
+                    satellites.Add(new Satellite(l1,l2,l3));
+                }
+                // Read the stream as a string, and write the string to the console.
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("The file could not be read:");
+            Console.WriteLine(e.Message);
+        }
+        Console.WriteLine(satellites.Count);
+        using (var ws = new WebSocket ("ws://localhost:1234/Commands")) {
+          ws.OnMessage += (sender, e) => {
+            Console.WriteLine ("Commands says: " + e.Data);
+            if(e.Data.StartsWith("READY")){
+              ws.Send($"COMMAND*{args[1]}*LES-5,CALSPHERE 1*1,0,0");//accel x
+            }else if(e.Data.StartsWith($"SECOND*{args[1]}")){
+
+            }else if(e.Data.StartsWith("SECOND")){
+              //find collisions small list 
+              var convergence_names = e.Data.Split("*")[2].Split(",");
+              Console.WriteLine(String.Join(" ",convergence_names));
+              Console.WriteLine(satellites.Where(x=>convergence_names.ToList().Contains(x.Name)).ToList().Count);
+              var collions = OrbitalCalculator.Services.Collisions.FindCollisions(satellites,satellites.Find(x=>x.Name.Trim()==e.Data.Split("*")[3]),2,60);
+              Console.WriteLine(collions);
+            }
+          };
+          ws.Connect ();
+          Console.WriteLine(String.Join(",",args));
+          if(args[0]=="0"){
+            ws.Send ($"GROUNDCONNECTED*{args[1]}");
+          }
+          while(true){
+            Thread.Sleep(25);
+          }
+        }
     }
   }
 }
--- SatelliteProgram/Program.cs
+++ SatelliteProgram/Program.cs
@@ -2,40 +2,40 @@
 using System.Collections.Generic;
 using WebSocketSharp;
 using WebSocketSharp.Server;
-
+using System.IO.Ports;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using SGPdotNET.CoordinateSystem;
+using SGPdotNET.Observation;
+using SGPdotNET.TLE;
+using SGPdotNET.Util;
+using System.IO;
 namespace Satellite
 {
   public class Commands : WebSocketBehavior
   {
-    public List<string> convergence_name;
-    public String satellite_name = "";
-    public bool waiting_confirmation = false;
+    public String satellite_name = "CALSPHERE 2";
     protected override void OnMessage (MessageEventArgs e)
     {
         if (e.Data.StartsWith("COMMAND"))
         {
-          if(waiting_confirmation){
             if(false){//if(trusted)
                 Send("Running CMD");
                 //output orbital change request
             }else{ 
                 // waiting for second confirmation or one time pass 
-                waiting_confirmation = true;
-                convergence_name = new List<string>();
-                convergence_name.InsertRange(0,e.Data.Split(" ")[1].Split(","));
-                Send("Getting Confirmation OR send override");
+              Send("Awaiting Confirmation OR send override");
+              Sessions.Broadcast($"SECOND*{e.Data.Split("*")[1]}*{e.Data.Split("*")[2]}*{satellite_name}");  
+              if(e.Data.Split("*").Length>4){
+                //process pad overide
+              }
             }
-          }else{
-            if(e.Data.Split(" ").Length>2){
-              //process pad overide
-            }
-          }
-        }else if(e.Data.StartsWith("SECONDREQUEST")){
-            var msg = "SECOND intercept {sat name} {time}";
-            Send("OK");
         }else if(e.Data.StartsWith("SECONDRESPONSE")){
             Send("OK");
-            if(e.Data.Split(" ")[1]=="TRUE"){
+            if(e.Data.Split("*")[1]=="TRUE"){
             //      take action ordered
             }else{
             //      reject and degrade trust model 
@@ -43,23 +43,23 @@ namespace Satellite
         } else if(e.Data.StartsWith("GROUNDCONNECTED")){
             //tell ground if any actions failed
             // add connection to trust model
-            if(convergence_name.Count>0){
-              Send($"CONVERGENCE {String.Join(",",convergence_name)} {satellite_name}");
-              convergence_name=new List<string>();
-            }else{
-              Send("OK");
-            }
+            string station_name = e.Data.Split("*")[1];
+              Send("READY*"+satellite_name);
         }
     }
   }
 
   public class Program
   {
+    public List<string> convergence_name;
+    public String satellite_name = "CALSPHERE 2";
+    public String pending_station_name = ""; 
+    public bool waiting_confirmation = false;
     public static void Main (string[] args)
     {
       var wssv = new WebSocketServer ("ws://localhost:1234");
-      wssv.AddWebSocketService<Commands> ("/Commands");
-      wssv.Start ();
+      wssv.AddWebSocketService<Commands>("/Commands");
+      wssv.Start();
       Console.ReadKey (true);
       wssv.Stop ();
     }
